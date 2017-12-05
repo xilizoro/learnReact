a78e03219501d6e5d1b2ecbfc2ed50db59184bcf
Gao, Xiang <xigao@webmd.net>
2017-12-05 14:12:13 -0500

    updated

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Header extends Component {
+    render () {
+        return (
+            <div>
+                <h1> React test </h1>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Header />,
+    document.getElementById('root')
+)
